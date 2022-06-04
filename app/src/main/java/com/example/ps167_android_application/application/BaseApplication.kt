9c2acc918f20f3b5a18284d697fbb408f722552f
taufiq <taufiq.goe@gmail.com>
2022-06-04 13:23:17 +0800

    Add Dependency Injection

---
+++ app/src/main/java/com/example/ps167_android_application/application/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.example.ps167_android_application.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
