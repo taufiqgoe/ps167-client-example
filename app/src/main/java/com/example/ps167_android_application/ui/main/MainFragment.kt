85fcdd82224b302395d46b5889148450b9339aa1
taufiq <taufiq.goe@gmail.com>
2022-06-04 12:10:18 +0800

    Add Main Screen Fragment

---
+++ app/src/main/java/com/example/ps167_android_application/ui/main/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.example.ps167_android_application.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.ps167_android_application.R
+import com.example.ps167_android_application.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val galleryViewModel =
+            ViewModelProvider(this)[MainViewModel::class.java]
+
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/ps167_android_application/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.ps167_android_application.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
